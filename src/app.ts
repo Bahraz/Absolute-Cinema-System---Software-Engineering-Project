5e6459c7eb9cea1230d37f77a42e7cff0e62b572
Jaroslaw <jarekg0626@gmail.com>
2026-01-04 15:57:04 +0100

    add express-rate-limit, helmet, cors

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,48 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import cors from "cors";
 import path from "path";
 import session from "express-session";
 import router from "@routes/index";
 import errorHandler from "@middlewares/error.middleware";
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+const allowedOrigins = ["http://localhost:3000"];
+
 const app = express();
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(helmet());
+app.use(limiter);
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS blocked"));
+      }
+    },
+  })
+);
+
 app.use(
   session({
     name: "sid",
-    secret: process.env.SESSION_SECRET || "dev_secret",
+    secret: process.env.JWT_SECRET || "dev_secret",
     resave: false,
     saveUninitialized: false,
     cookie: {
